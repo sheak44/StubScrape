87037c8927116320bbc3c49d365a83e38361efe8
John Spaw <jspaw@millikin.edu>
2016-01-12 13:42:34 -0800

    First commit

---
+++ scrape.py
@@ -0,0 +1,16 @@
+import json
+from pprint import pprint
+import urllib2
+
+
+url = 'http://www.stubhub.com/ticketAPI/restSvc/event/9341277'
+
+data = json.load(urllib2.urlopen(url))
+tickets = data['eventTicketListing']['eventTicket']
+
+prices = [ticket['tc']['amount'] for ticket in tickets]
+pprint(sorted(prices))
+
+print('WOO HOO')
+
+
---
+++ scratch.py
@@ -0,0 +1,28 @@
+#x = 5
+#ts = 'The value of x is %s' %(x)
+#print(ts)
+
+
+import json
+from pprint import pprint
+import urllib2
+
+eventKey=9159093
+
+url = 'http://www.stubhub.com/ticketAPI/restSvc/event/%s' %(eventKey)
+
+data = json.load(urllib2.urlopen(url))
+tickets = data['eventTicketListing']['eventTicket']
+
+priceIO = open('9159093_price.txt','a',1)
+
+prices = [ticket['cp']for ticket in tickets]
+
+for p in prices:
+	pr = '%s' %(p)
+	priceIO.write(pr+'\n')
+
+priceIO.close()
+
+#print(sorted(prices))
+
